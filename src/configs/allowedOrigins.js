4fb4be339eabb7ed275dc7818baa96979b6a84d3
yichien <kevinyichienlee@gmail.com>
2024-02-05 21:24:07 +0800

    production

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -3,8 +3,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3030",
   "http://localhost:3030",
-  "https://petzone-backend.zeabur.app",
-  "https://petzone-dev.zeabur.app",
+  "https://delta-challenge.zeabur.app",
+  "https://delta-challenge-frontend.zeabur.app",
 ];
 
 module.exports = allowedOrigins;
